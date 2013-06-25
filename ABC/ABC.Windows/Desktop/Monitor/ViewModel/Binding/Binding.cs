33c640d1c67c4d92dd2ba4a3fc60a32fcbc32567
Steven Jeuris <steven.jeuris@gmail.com>
2013-06-25 16:22:33 +0200

    Added VDM Monitor client.

---
+++ ABC/ABC.Windows/Desktop/Monitor/ViewModel/Binding/Binding.cs
@@ -0,0 +1,17 @@
+﻿namespace ABC.Windows.Desktop.Monitor.ViewModel.Binding
+{
+	public enum Properties
+	{
+		VirtualDesktops,
+		WindowClipboard,
+		SelectedDesktop
+	}
+
+	public enum Commands
+	{
+		Show,
+		Hide,
+		Cut,
+		Refresh
+	}
+}
