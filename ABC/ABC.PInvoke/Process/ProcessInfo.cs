31afabfa43181fbb79e736c776456bf092f18e5a
Steven Jeuris <steven.jeuris@gmail.com>
2014-01-16 12:02:52 +0100

    Moved ProcessTracker from Laevo to ABC toolkit.

---
+++ ABC/ABC.PInvoke/Process/ProcessInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace ABC.PInvoke.Process
+{
+	public class ProcessInfo
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public string CommandLine { get; private set; }
+
+
+		public ProcessInfo( int id, string name, string commandLine )
+		{
+			Id = id;
+			Name = name;
+			CommandLine = commandLine;
+		}
+	}
+}
