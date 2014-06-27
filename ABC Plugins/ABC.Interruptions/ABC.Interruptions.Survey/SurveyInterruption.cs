b406d85d6f028d642fbe6b0d7ecd44b18e8ac5fb
Dominik Grondziowski <dominik.grondziowski@gmail.com>
2014-06-27 13:25:31 +0200

    Survey interruptions.

---
+++ ABC Plugins/ABC.Interruptions/ABC.Interruptions.Survey/SurveyInterruption.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace ABC.Interruptions.Survey
+{
+	[DataContract]
+	internal class SurveyInterruption : AbstractInterruption
+	{
+		[DataMember]
+		readonly string _link;
+
+		/// <summary>
+		/// Id of user who interruption is addessed to, all is default value- every user will be notified.
+		/// </summary>
+		[DataMember]
+		public string TargetUserId { get; private set; }
+
+
+		public SurveyInterruption(string name, string link, string targetUserId)
+			: base(name)
+		{
+			_link = link;
+			TargetUserId = targetUserId;
+		}
+
+
+		protected override void OnInterruptionOpened()
+		{
+			ServiceProvider.Browser.OpenWebsite(_link);
+		}
+	}
+}
