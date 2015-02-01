0f402082c856241c29130ba79741ed0fe473d026
Steven Jeuris <steven.jeuris@gmail.com>
2015-02-01 17:38:53 +0100

    Fixed incorrect merge of TestWorkspace.

--- ABC/ABC.Tests/Workspaces/TestWorkspace.cs
+++ ABC/ABC.Tests/Workspaces/TestWorkspace.cs
@@ -2,16 +2,6 @@
 {
 	class TestWorkspace : AbstractWorkspace<TestSession>
 	{
-		protected override void ShowInner()
-		{
-			throw new System.NotImplementedException();
-		}
-
-		protected override void HideInner()
-		{
-			throw new System.NotImplementedException();
-		}
-
 		public override bool HasResourcesToSuspend()
 		{
 			return false;
