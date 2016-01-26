f2f34e5df94125af8fb188959420e6333970db17
Steven Jeuris <steven.jeuris@gmail.com>
2016-01-26 18:07:00 +0100

    Upgrade to Visual Studio 2015.

--- ABC/ABC/Properties/AssemblyInfo.cs
+++ ABC/ABC/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ABC.Common")]
+[assembly: AssemblyTitle("ABC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ABC.Common")]
+[assembly: AssemblyProduct("ABC")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
